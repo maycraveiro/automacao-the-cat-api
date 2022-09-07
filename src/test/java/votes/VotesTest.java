0f9f43d386d373f0f590e0353dab9a0bca5a3a55
Mayara Craveiro <mayara_craveiro@hotmail.com>
2022-09-07 20:03:24 -0300

    Versão inicial do projeto de automação

---
+++ src/test/java/votes/VotesTest.java
@@ -0,0 +1,139 @@
+package votes;
+
+import io.restassured.http.ContentType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class VotesTest {
+
+    private Integer voteId;
+
+    @BeforeClass
+    public static void requestURL(){
+        baseURI = "https://api.thecatapi.com";
+        basePath = "/v1/votes";
+    }
+
+    @Test
+    public void testDadoUmUsuarioQuandoVotaEmGostarDaImagemEntaoObtemStatusCode201() {
+
+        voteId = given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .body("{\n" +
+                        "  \"image_id\": \"asf2\",\n" +
+                        "  \"sub_id\": \"my-user-1234\",\n" +
+                        "  \"value\": 0\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .body("message", equalTo("SUCCESS"))
+                .assertThat()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+        System.out.println(voteId);
+    }
+
+    @Test
+    public void testDadoUsuarioQuandoPesquisaVotoExistenteEntaoObtemStatusCode200() {
+
+        Integer voteId = given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .body("{\n" +
+                        "  \"image_id\": \"asf2\",\n" +
+                        "  \"sub_id\": \"my-user-1234\",\n" +
+                        "  \"value\": 0\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .body("message", equalTo("SUCCESS"))
+                .assertThat()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+        System.out.println(voteId);
+
+        given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .pathParam("voteId", voteId)
+                .when()
+                .get("/{voteId}")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testDadoUsuarioQuandoPesquisaVotoInexistenteEntaoObtemStatusCode404() {
+
+        given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .pathParam("voteId", 599035)
+                .when()
+                .get("/{voteId}")
+                .then()
+                .log().all()
+                .body(equalTo("NOT_FOUND"))
+                .assertThat()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testDadoUsuarioQuandoExcluiVotoExistenteEntaoObtemStatusCode200() {
+
+        Integer voteId = given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .body("{\n" +
+                        "  \"image_id\": \"asf2\",\n" +
+                        "  \"sub_id\": \"my-user-1234\",\n" +
+                        "  \"value\": 1\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .log().all()
+                .extract()
+                .path("id");
+
+        System.out.println(voteId);
+
+        given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .pathParam("voteId", voteId)
+                .when()
+                .delete("/{voteId}")
+                .then()
+                .log().all()
+                .body("message", equalTo("SUCCESS"))
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testDadoUsuarioQuandoExcluiVotoInexistenteEntaoObtemStatusCode404() {
+
+        given()
+                .header("x-api-key", "live_Ln3MwKXoB0kSSrAd7SqEBJEoDQKyEmwfYIsyoPVuTkXjkteglhNp1JfVo2W6PczH")
+                .pathParam("voteId", 599035)
+                .when()
+                .delete("/{voteId}")
+                .then()
+                .log().all()
+                .body(equalTo("NO_VOTE_FOUND_MATCHING_ID"))
+                .assertThat()
+                .statusCode(404);
+    }
+}
+
